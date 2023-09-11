af5d552e30ec374bce57cacfa44e5af09259bb37
yoonnholo <sbstony@snu.ac.kr>
2023-09-11 13:24:50 +0900

    i changed our

--- Untitled.R
+++ Untitled.R
@@ -2,3 +2,12 @@ AA<-100
 ##https://github.com/yoonnholo/Myuri
 
 spicenwolf<-c("Holo","Myuri","Law")
+str(spicenwolf)
+A<-c("Yoon","Han","Bin")
+B<-c(6,9,15)
+C<-c("F","F","M")
+D<-data.frame(Name=A,Dick=B,Sex=C)
+D
+GodAnime=spicenwolf
+View(hololo)
+getwd()

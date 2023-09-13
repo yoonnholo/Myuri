51cc731f08c76d3bc37762303b0d33e0fab1a927
yoonnholo <sbstony@snu.ac.kr>
2023-09-13 17:52:03 +0900

    Update Class 01.R

--- Class 01.R
+++ Class 01.R
@@ -1,2 +1,12 @@
 ## 09. 11. Class ##
 ## 교수님 코드로 수업을 진행하기 때문에 이 스크립트는 폐쇄합니다 ##
+
+## 팩터 ##
+
+fetish<-c("Bondage", "Dominance","Sadism","Masochism")
+LG<-c("Bondage", "Bondage", "Sadism")
+factor(LG)
+factor(LG,levels<-(fetish))
+
+Grade<-c("1","2","3","4")
+factor(Grade,levels<-c("1","2","3","4","5","6"))

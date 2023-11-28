d27130b45317a4747efff8b8f875b29450d92efa
yoonnholo <sbstony@snu.ac.kr>
2023-11-28 14:27:20 +0900

    ㄷㄷ

--- sononmum.R
+++ sononmum.R
@@ -7,9 +7,9 @@ data %>%
   select(HID,PID,YGENDERw4,YDLQ2A01w4,YMDA1C01w4:YMDA1C15w4,YDLQ1A01w4:YDLQ1A15w4,YPSY4B01w4:YPSY4B06w4) %>% 
   rowwise() %>%
   mutate(SmartPhone = sum(c_across(starts_with("YMDA1C01w4"):starts_with("YMDA1C15w4")), na.rm = TRUE)) %>% 
-  mutate(Real = sum(c_across(starts_with("YDLQ1A01w4"):starts_with("YDLQ1A15w4")), na.rm = TRUE)) %>% 
-  mutate(Agressive = sum(c_across(starts_with("YPSY4B01w4"):starts_with("YPSY4B06w4")), na.rm = TRUE)) %>% 
-  rename(Cyber_Fly = YDLQ2A01w4) %>% 
+  mutate(Real_Misdeed = sum(c_across(starts_with("YDLQ1A01w4"):starts_with("YDLQ1A15w4")), na.rm = TRUE)) %>% 
+  mutate(Agression = sum(c_across(starts_with("YPSY4B01w4"):starts_with("YPSY4B06w4")), na.rm = TRUE)) %>% 
+  rename(Cyber_Misdeed = YDLQ2A01w4) %>% 
   rename(Gender = YGENDERw4) %>% 
   rename(HomeID = HID) %>% 
   rename(PersonalID = PID) %>% 
@@ -18,10 +18,9 @@ data %>%
   select(-starts_with("YPSY4B01w4"): -starts_with("YPSY4B06w4")) %>% 
   na.omit() -> new
 
-table(new$Cyber_Fly)
-table(new$Real)
-table(new$SmartPhone)
-table(new$Agressive)
+view(new)
 
-table(new$HomeID)
-table(new$PersonalID)
+table(new$Cyber_Misdeed)
+table(new$SmartPhone)
+table(new$Real_Misdeed)
+table(new$Agression)

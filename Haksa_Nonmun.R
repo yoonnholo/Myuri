15eaacc5c5eb11415850583b0ce3a12c0366c1ba
yoonnholo <sbstony@snu.ac.kr>
2024-10-31 17:46:07 +0900

    ㅁㄴㅇ

--- Haksa_Nonmun.R
+++ Haksa_Nonmun.R
@@ -55,20 +55,116 @@ new_data$Turnover <- rowSums(new_data[, c("Turnover1", "Turnover2")], na.rm = TR
 
 hist(new_data$MZ)
 table(new_data$MZ)
+new_data$IsZGen <- ifelse(new_data$MZ >= 1995 & new_data$MZ <= 2004, 1, 0)
+
+##########################################################################
+
+# 직무 만족에 대한 회귀 분석
+satisfaction_model <- lm(Satisfaction ~ Hierarchical, data = new_data)
+summary(satisfaction_model)
+
+# 조직 몰입에 대한 회귀 분석
+commitment_model <- lm(Commitment ~ Hierarchical, data = new_data)
+summary(commitment_model)
+
+# 이직 의도에 대한 회귀 분석
+turnover_model <- lm(Turnover ~ Hierarchical, data = new_data)
+summary(turnover_model)
+
+##########################################################################
+
+# 매개효과 분석
+
+# 1단계: 독립변수 -> 종속변수
+satisfaction_model <- lm(Satisfaction ~ Hierarchical, data = new_data)
+commitment_model <- lm(Commitment ~ Hierarchical, data = new_data)
+turnover_model <- lm(Turnover ~ Hierarchical, data = new_data)
+
+# 2단계: 독립변수 -> 매개변수
+mediator_model <- lm(LowCommunicate ~ Hierarchical, data = new_data)
+
+# 3단계: 매개변수 -> 종속변수
+satisfaction_mediator <- lm(Satisfaction ~ LowCommunicate, data = new_data)
+commitment_mediator <- lm(Commitment ~ LowCommunicate, data = new_data)
+turnover_mediator <- lm(Turnover ~ LowCommunicate, data = new_data)
+
+# 4단계: 독립변수와 매개변수를 모두 포함
+satisfaction_full <- lm(Satisfaction ~ Hierarchical + LowCommunicate, data = new_data)
+commitment_full <- lm(Commitment ~ Hierarchical + LowCommunicate, data = new_data)
+turnover_full <- lm(Turnover ~ Hierarchical + LowCommunicate, data = new_data)
+
+# 결과 확인
+summary(satisfaction_model)
+summary(commitment_model)
+summary(turnover_model)
+summary(mediator_model)
+summary(satisfaction_mediator)
+summary(commitment_mediator)
+summary(turnover_mediator)
+summary(satisfaction_full)
+summary(commitment_full)
+summary(turnover_full)
+
+##########################################################################
+
+# Z세대 데이터 필터링
+z_gen_data <- subset(new_data, IsZGen == 1)
+
+# 1단계: 독립변수 -> 종속변수
+satisfaction_model <- lm(Satisfaction ~ Hierarchical, data = z_gen_data)
+commitment_model <- lm(Commitment ~ Hierarchical, data = z_gen_data)
+turnover_model <- lm(Turnover ~ Hierarchical, data = z_gen_data)
+
+# 2단계: 독립변수 -> 매개변수
+mediator_model <- lm(LowCommunicate ~ Hierarchical, data = z_gen_data)
+
+# 3단계: 매개변수 -> 종속변수
+satisfaction_mediator <- lm(Satisfaction ~ LowCommunicate, data = z_gen_data)
+commitment_mediator <- lm(Commitment ~ LowCommunicate, data = z_gen_data)
+turnover_mediator <- lm(Turnover ~ LowCommunicate, data = z_gen_data)
+
+# 4단계: 독립변수와 매개변수를 모두 포함
+satisfaction_full <- lm(Satisfaction ~ Hierarchical + LowCommunicate, data = z_gen_data)
+commitment_full <- lm(Commitment ~ Hierarchical + LowCommunicate, data = z_gen_data)
+turnover_full <- lm(Turnover ~ Hierarchical + LowCommunicate, data = z_gen_data)
+
+# 결과 확인
+summary(satisfaction_model)
+summary(commitment_model)
+summary(turnover_model)
+summary(mediator_model)
+summary(satisfaction_mediator)
+summary(commitment_mediator)
+summary(turnover_mediator)
+summary(satisfaction_full)
+summary(commitment_full)
+summary(turnover_full)
 
 
 
-set.seed(123)  # 재현 가능성을 위해 시드 설정
+# 직무 만족에 대한 조절효과 확인
+satisfaction_mz_model <- lm(Satisfaction ~ Hierarchical * IsZGen, data = new_data)
+summary(satisfaction_mz_model)
 
-# 비Z그룹 데이터 필터링
-non_z_data <- new_data[new_data$IsZGen == 0, ]
+# 조직 몰입에 대한 조절효과 확인
+commitment_mz_model <- lm(Commitment ~ Hierarchical * IsZGen, data = new_data)
+summary(commitment_mz_model)
 
-# 573개의 표본을 랜덤으로 추출하고 분포 검토
-sample_non_z <- non_z_data[sample(nrow(non_z_data), 573), ]
+# 이직 의도에 대한 조절효과 확인
+turnover_mz_model <- lm(Turnover ~ Hierarchical * IsZGen, data = new_data)
+summary(turnover_mz_model)
 
-# 대표성 확인: 주요 변수 평균 비교
-colMeans(sample_non_z[, c("Hierarchical", "LowCommunicate", "Satisfaction", "Commitment", "Turnover")])
-colMeans(non_z_data[, c("Hierarchical", "LowCommunicate", "Satisfaction", "Commitment", "Turnover")])
+
+# 매개변수 LowCommunicate와 조절변수 IsZGen의 상호작용 포함
+satisfaction_mediation_mod <- lm(Satisfaction ~ Hierarchical * IsZGen + LowCommunicate * IsZGen, data = new_data)
+commitment_mediation_mod <- lm(Commitment ~ Hierarchical * IsZGen + LowCommunicate * IsZGen, data = new_data)
+turnover_mediation_mod <- lm(Turnover ~ Hierarchical * IsZGen + LowCommunicate * IsZGen, data = new_data)
+
+# 결과 확인
+summary(satisfaction_mediation_mod)
+summary(commitment_mediation_mod)
+summary(turnover_mediation_mod)
+
 
 
 
